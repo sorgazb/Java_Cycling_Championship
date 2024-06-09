cef8bee51fa003ef737aa1e71892bc7703bc84b6
sorgazb <sorgazb@gmail.com>
2024-06-09 23:52:37 +0200

    Commit ciclista

--- Java_Cycling_Championship/src/demo/Ciclista.java
+++ Java_Cycling_Championship/src/demo/Ciclista.java
@@ -1,6 +1,6 @@
 package demo;
 
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Ciclista {
@@ -12,6 +12,26 @@ public class Ciclista {
 	private Map<String, Double> resultados;
 	
 	
+	
+	public Ciclista(String nombreCiclista, Bicicleta bicicleta, double habilidad, double energia) {
+		this.nombreCiclista = nombreCiclista;
+		this.bicicleta = bicicleta;
+		this.habilidad = habilidad;
+		this.energia = energia;
+		this.resultados = new HashMap<String, Double>();
+	}
+	public String getNombreCiclista() {
+		return nombreCiclista;
+	}
+	public void setNombreCiclista(String nombreCiclista) {
+		this.nombreCiclista = nombreCiclista;
+	}
+	public Bicicleta getBicicleta() {
+		return bicicleta;
+	}
+	public void setBicicleta(Bicicleta bicicleta) {
+		this.bicicleta = bicicleta;
+	}
 	public double getHabilidad() {
 		return habilidad;
 	}
@@ -23,6 +43,14 @@ public class Ciclista {
 	}
 	public void setEnergia(double energia) {
 		this.energia = energia;
+	}
+	
+	public boolean haAbandonado() {
+		boolean haAbandonado=false;
+		if(energia<=0) {
+			haAbandonado=true;
+		}
+		return haAbandonado;
 	}
 	
 	
