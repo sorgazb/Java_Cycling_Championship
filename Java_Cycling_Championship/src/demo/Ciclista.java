f31b464397a77750a0bea5f37fbc49b6ca86993f
sorgazb <sorgazb@gmail.com>
2024-06-10 13:15:29 +0200

    commit ciclista

--- Java_Cycling_Championship/src/demo/Ciclista.java
+++ Java_Cycling_Championship/src/demo/Ciclista.java
@@ -5,14 +5,14 @@ import java.util.Map;
 
 public class Ciclista {
 	
+	//Atributos de la Clase Ciclista:
 	private String nombreCiclista;
 	private Bicicleta bicicleta;
 	private double habilidad;
 	private double energia;
 	private Map<String, Double> resultados;
 	
-	
-	
+	//Constructor de la Clase Ciclista:
 	public Ciclista(String nombreCiclista, Bicicleta bicicleta, double habilidad, double energia) {
 		this.nombreCiclista = nombreCiclista;
 		this.bicicleta = bicicleta;
@@ -20,6 +20,8 @@ public class Ciclista {
 		this.energia = energia;
 		this.resultados = new HashMap<String, Double>();
 	}
+	
+	//Metodos Getters & Setters de la Clase Ciclista:
 	public String getNombreCiclista() {
 		return nombreCiclista;
 	}
@@ -45,13 +47,39 @@ public class Ciclista {
 		this.energia = energia;
 	}
 	
+	//Funcionalidades de la Clase Ciclista:
+	
+	//Metodo que nos informa si el ciclista ha abandonado o no:
 	public boolean haAbandonado() {
 		boolean haAbandonado=false;
 		if(energia<=0) {
 			haAbandonado=true;
+			setEnergia(0);
 		}
 		return haAbandonado;
 	}
 	
+	//Metodo que nos devuelve el resultado de una Etapa en concreto:
+	public void obtenerResultadoEtapa(Etapa e) {
+		for(String etapa:resultados.keySet()) {
+			if(etapa.equals(e.getNombreEtapa())) {
+				System.out.println("Carrera("+etapa+") - Tiempo: "+resultados.get(etapa));
+			}
+		}
+	}
+	
+	public void usarBicicletaEtapa(Bicicleta b, Etapa e) {
+		double tiempo=b.tiempoFinalizarEtapa(this, e);
+		setEnergia(getEnergia()-tiempo);
+		if(haAbandonado()==true) {
+			resultados.put(e.getNombreEtapa(), tiempo);
+		}else {
+			if(resultados.isEmpty()) {
+				resultados.put(e.getNombreEtapa(), tiempo);
+			}else {
+				resultados.put(e.getNombreEtapa(), tiempo);
+			}
+		}
+	}
 	
 }
